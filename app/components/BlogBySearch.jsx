ddd50f885f4385b5e51d3c1eec19af4efea8d96e
layaCoder <412046213@qq.com>
2019-03-22 11:09:01 +0800

    add BlogBySearch page

---
+++ app/components/BlogBySearch.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class BlogBySearch extends Component {
+    componentDidMount() {
+    }
+
+    render() {
+        let myStyle = {
+            textAlign: 'center'
+        }
+        return (
+            <div>
+                <h2>Search Result</h2>
+            </div>
+        )
+    }
+}
