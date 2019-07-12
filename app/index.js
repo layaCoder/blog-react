35997d7be9d5bf0750be9480dd957be236d88e12
laya <412046213@qq.com>
2019-07-12 16:59:07 +0800

    cancel redux dev tool

--- app/index.js
+++ app/index.js
@@ -8,10 +8,12 @@ import blogApp from './store/reducers.js'
 
 
 /* composeEnhancers 启用redux devtool调试工具 ,项目打包时需要关闭，否则手机端无法正常打开页面*/
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+/*const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
 const store = createStore(blogApp,
     composeEnhancers(applyMiddleware(thunk))
-);
+);*/
+
+const store = createStore(blogApp,applyMiddleware(thunk));
 
 
 ReactDom.render(
